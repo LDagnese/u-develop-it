82fcfc58eaadb5d7f20040b57f473ecbea6b6284
loudagnese <louis.dagnese@gmail.com>
2022-07-17 17:54:55 -0700

    add CRUD methods to candidates table

--- server.js
+++ server.js
@@ -20,8 +20,36 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-db.query(`SELECT * FROM candidates`, (err,rows) => {
-    console.log(rows);
+// db.query(`SELECT * FROM candidates`, (err,rows) => {
+//     console.log(rows);
+// });
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id =1`,(err,row) => {
+//     if (err) {
+//         console.log(err);
+//     } 
+//     console.log(row);
+// });
+
+// DELETE a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`,1,(err,result)=> {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// CREATE a candidate
+const sql = `INSERT INTO candidates (id,first_name,last_name,industry_connected)
+            VALUES (?,?,?,?)`;
+const params = [1,'Ronald','Firbank',1];
+
+db.query(sql,params, (err,result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
 });
 
 app.use((req, res) => {
