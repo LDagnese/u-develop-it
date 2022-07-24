e0c91b02970118e2b7fbf33324a65661b1635aba
loudagnese <louis.dagnese@gmail.com>
2022-07-24 15:55:12 -0700

    finish votes route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use(require('./candidatesRoutes'));
 router.use(require('./partyRoutes'));
 router.use(require('./voterRoutes'));
+router.use(require('./voteRoutes'));
 
 module.exports = router;
---
+++ routes/apiRoutes/voteRoutes.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
+
+// POST a vote
+router.post('/vote', ({ body }, res) => {
+    const errors = inputCheck(body, 'voter_id', 'candidate_id');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `INSERT INTO votes (voter_id, candidate_id) VALUES (?,?)`;
+    const params = [body.voter_id, body.candidate_id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body,
+            changes: result.affectedRows
+        });
+    });
+});
+
+// COUNT the votes
+router.get('/votes', (req, res) => {
+    const sql = `SELECT candidates.*, parties.name AS party_name, COUNT(candidate_id) AS count
+                FROM votes
+                LEFT JOIN candidates ON votes.candidate_id = candidates.id
+                LEFT JOIN parties ON candidates.party_id = parties.id
+                GROUP BY candidate_id ORDER BY count DESC;`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+module.exports = router;
