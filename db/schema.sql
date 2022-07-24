d516b348068bbb0e3b003e387a898ce9c05c8d66
loudagnese <louis.dagnese@gmail.com>
2022-07-24 11:42:12 -0700

    add parties table and parties

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,9 @@ CREATE TABLE candidates (
     last_name VARCHAR(30) NOT NULL,
     industry_connected BOOLEAN NOT NULL
 );
+
+CREATE TABLE parties (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT
+);
